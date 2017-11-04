e20d395983c72f7c2be5e5da659ae609bf4c1211
Andre <elij.richards22@gmail.com>
2017-11-04 17:47:29 -0400

    test

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,7 +10,7 @@ import { EMPTY_CART } from './types'
 
 export const fetchUser = () => async dispatch => {
     const res = await axios.get(API_URL+'/api/current_user')
-    console.log(res.data)
+    console.log(res)
     dispatch({ type: FETCH_USER, payload: res.data })
 }
 // export const fetchUser = () => {
