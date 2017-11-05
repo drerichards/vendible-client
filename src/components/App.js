3eb92ef2c5eb27f33d6f1318e3cfeb4542f98e2e
Raymond Smith <raysmithdev@gmail.com>
2017-11-05 10:41:26 -0800

    1

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,12 @@ import React, {Component} from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import {connect} from 'react-redux'
 import * as actions from '../actions'
-import createBrowserHistory from 'history/createBrowserHistory'
 
 import NavBar from './NavBar'
 import Landing from './Landing'
 import Departments from './Departments'
 import ProductDisplay from './Product_Display'
 import ShoppingCart from './Shopping_Cart'
-const history = createBrowserHistory()
 
 class App extends Component {
     render() {
@@ -19,7 +17,7 @@ class App extends Component {
                     <div>
                         <NavBar/>
                         <Route exact path='/' component={Landing}/>
-                        <Route exact path='/:userId/userEmail' component={Landing}/>
+                        <Route exact path='/:userId' component={Landing}/>
                         <Route path='/departments' component={Departments}/>
                         <Route path='/product_display' component={ProductDisplay}/>
                         <Route path='/shopping_cart' component={ShoppingCart}/>
