6017369e7169144f38d3ac3561a64382d2ef3c59
Andre <elij.richards22@gmail.com>
2017-11-07 02:02:37 -0500

    css refactor

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -48,8 +48,6 @@ class NavBar extends Component {
 
                     <div className='collapse navbar-collapse' id='navbarSupportedContent'>
                         <ul id='navlink' className='navbar-nav mr-auto' onClick={() => this.props.dispatch(hideModal())}>
-                            <li className='nav-item active'>
-                                <a className='nav-link' href='/'>Home<span className='sr-only'>(current)</span></a></li>
                             <li className='nav-item'><a className='nav-link' href='/'>About</a></li>
                             <li className='nav-item'><a className='nav-link' href='/departments'>Shop</a></li>
                             <li className='nav-item'><a className='nav-link' href='/'>Contact</a></li>
