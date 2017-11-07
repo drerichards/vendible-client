5d18327a0a9db6e616e928f89048126586f123f2
Andre <elij.richards22@gmail.com>
2017-11-07 00:38:50 -0500

    css refactor

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -86,7 +86,7 @@ class Landing extends Component {
                 </header>
             </main>
             <section className="addlCaption">
-                <div className="mobileCaption carousel-caption">
+                <div className="mobileCaption">
                     <h1>Whatever you’ve got in mind</h1>
                     <h3>We’ve got inside!</h3>
                     <button type="button" className="btn btn-primary">
