e23e4a99fd3e21f289263d13954c3242601dce7a
Andre <elij.richards22@gmail.com>
2017-11-07 23:21:29 -0500

    css refactor

--- src/components/Shopping_Cart.js
+++ src/components/Shopping_Cart.js
@@ -36,7 +36,7 @@ class ShoppingCart extends Component {
                 </ul>
                 <div>
                     <Payment total={total} />
-                    <h5>Subtotal: <strong>${total}</strong></h5>
+                    <h5>Subtotal:${total}</h5>
                 </div>
             </div>
         )
