fa56ed02ff5a960e8bbe7921480acb74d932a893
Andre <elij.richards22@gmail.com>
2018-01-11 21:28:38 -0500

    localstorage

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,8 +1,8 @@
-import { ON_LOGOUT } from '../actions/types'
+import { ON_LOGOUT, FETCH_USER_SUCCESS } from '../actions/types'
 
 export default function (state = {}, action) {
     switch (action.type) {
-        case 'FETCH_USER_SUCCESS': {
+        case FETCH_USER_SUCCESS: {
           return {
             ...state,
             id: action.id,
